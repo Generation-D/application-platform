9acd77b873ac70605b4c57340bc5b772b9eb9e2b
Interperle <marib.aldoais@googlemail.com>
2024-06-15 17:47:32 +0200

    prettier

--- frontend/src/app/403/page.tsx
+++ frontend/src/app/403/page.tsx
@@ -4,10 +4,10 @@ import Logger from "@/logger/logger";
 const log = new Logger("Custom403");
 
 const Custom403: React.FC = async () => {
-  const supabase = initSupabaseActions()
+  const supabase = initSupabaseActions();
   const error = await supabase.auth.signOut();
-  if (error){
-    log.error(JSON.stringify(error))
+  if (error) {
+    log.error(JSON.stringify(error));
   }
   return (
     <div>
--- frontend/src/components/applicantSettings.tsx
+++ frontend/src/components/applicantSettings.tsx
@@ -12,56 +12,56 @@ import Popup from "@/components/layout/popup";
 import { supabase } from "@/utils/supabaseBrowserClient";
 
 export default function ApplicantSettings() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [user, setUser] = useState<User | null>(null);
-    const [isPopupOpen, setPopupOpen] = useState(false);
-  
-    useEffect(() => {
-      const fetchUser = async () => {
-        const { data, error } = await supabase.auth.getUser();
-        if (!error && data) {
-          setUser(data.user);
-        }
-        setIsLoading(false);
-      };
-      fetchUser();
-    }, []);
-  
-    const togglePopup = () => {
-      setPopupOpen(!isPopupOpen);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data, error } = await supabase.auth.getUser();
+      if (!error && data) {
+        setUser(data.user);
+      }
+      setIsLoading(false);
     };
+    fetchUser();
+  }, []);
+
+  const togglePopup = () => {
+    setPopupOpen(!isPopupOpen);
+  };
   return (
     <div className="flex flex-col items-start justify-between space-y-4">
-        <h1 className="text-2xl font-bold mb-4">Einstellungen</h1>
-        <h1 className="text-2xl font-bold mb-4">Einstellungen</h1>
-        {isPopupOpen && (
-          <Popup onClose={togglePopup}>
-            <SubmitDeletionForm email={user?.email || ""} />
-          </Popup>
-        )}
-        <div>
-          <label>Email: {Awaiting(isLoading, user?.email)}</label>
-        </div>
-        <h3 className="py-2 text-xl">Ändere dein Passwort</h3>
-        <div>
-          <h2>
-            Dir wird zum Zurücksetzen deines Passworts an die oben genannte
-            Email-Adresse ein Link gesendet.
-          </h2>
-          {user?.email == undefined ? (
-            <CircularProgress size={"1rem"} />
-          ) : (
-            <SendPasswordResetForm email={user!.email!} />
-          )}
-        </div>
-        <h4 className="py-2 text-xl mb-3">Lösche deinen Account</h4>
-        <button
-          type="submit"
-          className="apl-alert-button-fixed-big"
-          onClick={togglePopup}
-        >
-          Account löschen
-        </button>
+      <h1 className="text-2xl font-bold mb-4">Einstellungen</h1>
+      <h1 className="text-2xl font-bold mb-4">Einstellungen</h1>
+      {isPopupOpen && (
+        <Popup onClose={togglePopup}>
+          <SubmitDeletionForm email={user?.email || ""} />
+        </Popup>
+      )}
+      <div>
+        <label>Email: {Awaiting(isLoading, user?.email)}</label>
       </div>
+      <h3 className="py-2 text-xl">Ändere dein Passwort</h3>
+      <div>
+        <h2>
+          Dir wird zum Zurücksetzen deines Passworts an die oben genannte
+          Email-Adresse ein Link gesendet.
+        </h2>
+        {user?.email == undefined ? (
+          <CircularProgress size={"1rem"} />
+        ) : (
+          <SendPasswordResetForm email={user!.email!} />
+        )}
+      </div>
+      <h4 className="py-2 text-xl mb-3">Lösche deinen Account</h4>
+      <button
+        type="submit"
+        className="apl-alert-button-fixed-big"
+        onClick={togglePopup}
+      >
+        Account löschen
+      </button>
+    </div>
   );
 }
