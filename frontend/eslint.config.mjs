252d520942c67c5a07a182bdcf7f71fbb1dea08d
fkropfhamer <fabian.kropfhamer@gmail.com>
2025-10-05 14:22:07 +0200

    update linting

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,16 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
-      "react-hooks/exhaustive-deps": "off",
-    },
-  }),
-];
+const eslintConfig = [{
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}, ...compat.config({
+  extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unused-expressions": "off",
+    "react-hooks/exhaustive-deps": "off",
+  },
+})];
 
 export default eslintConfig;
