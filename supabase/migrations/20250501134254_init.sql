a0999e858cb18386aa0aa9a60423f3674c7c5f1d
Simon Engstler <SimonEngstler@live.de>
2026-02-14 22:35:45 +0800

    fix sonarqube issue

--- supabase/migrations/20250501134254_init.sql
+++ supabase/migrations/20250501134254_init.sql
@@ -3746,6 +3746,44 @@ using ((EXISTS ( SELECT 1
   WHERE ((user_profiles_table.userid = auth.uid()) AND (user_profiles_table.userrole = 2)))));
 
 
-create view public.users as select * from auth.users;
+create view public.users as
+select
+  instance_id,
+  id,
+  aud,
+  role,
+  email,
+  encrypted_password,
+  email_confirmed_at,
+  invited_at,
+  confirmation_token,
+  confirmation_sent_at,
+  recovery_token,
+  recovery_sent_at,
+  email_change_token_new,
+  email_change,
+  email_change_sent_at,
+  last_sign_in_at,
+  raw_app_meta_data,
+  raw_user_meta_data,
+  is_super_admin,
+  created_at,
+  updated_at,
+  phone,
+  phone_confirmed_at,
+  phone_change,
+  phone_change_token,
+  phone_change_sent_at,
+  confirmed_at,
+  email_change_token_current,
+  email_change_confirm_status,
+  banned_until,
+  reauthentication_token,
+  reauthentication_sent_at,
+  is_sso_user,
+  deleted_at,
+  is_anonymous
+from auth.users;
+
 revoke all on public.users from anon, authenticated;
 
