d4c9c2d4a659023bec3ba92b2231c01969cfd57b
faby <fabian.kropfhamer@gmail.com>
2026-05-15 18:38:21 +0200

    update phase and questions when no previous questions in supabase

--- backend/backend/process_config.py
+++ backend/backend/process_config.py
@@ -1,5 +1,6 @@
 import argparse
 from datetime import datetime
+import uuid
 
 from loguru import logger
 
@@ -116,11 +117,17 @@ def process_config(config_file_path: str, env_file_path: str | None = None):
             supabase.table('phase_table').select('phaseid, phasename').eq('phasename', phase_name).execute()
         )
 
-        if existing_phases.data and len(existing_phases.data) > 0:
-            log.info(f'Phase {phase_name} already exists, skipping...')
-            log.debug(str(existing_phases.data))
-            continue
+        phase_id = str(uuid.uuid4())
 
+        if existing_phases.data and len(existing_phases.data) > 0:            
+            phase_id = existing_phases.data[0]['phaseid']
+            exisiting_phase_questions = supabase.table('question_table').select('*').eq('phaseid', phase_id).execute()
+
+            if exisiting_phase_questions.data and len(exisiting_phase_questions.data) > 0:
+                log.info(f'Phase {phase_name} already exists, skipping...')
+                log.debug(str(existing_phases.data))
+                continue
+    
         data_phase_table = create_data_phase_table(
             phase_name,
             phase['phaseLabel'],
@@ -129,9 +136,10 @@ def process_config(config_file_path: str, env_file_path: str | None = None):
             phase['endDate'],
             'sections' in phase,
         )
-        log.info(f'Creating new Phase {phase_name}')
-
-        response_phase_table = supabase.table('phase_table').insert(data_phase_table).execute()
+        data_phase_table['phaseid'] = phase_id
+        
+        log.info(f'Creating new Phase {phase_name}')   
+        response_phase_table = supabase.table('phase_table').upsert(data_phase_table).execute()
         phase_sections = {}
         phase_id = response_phase_table.data[0]['phaseid']
         if 'sections' in phase:
