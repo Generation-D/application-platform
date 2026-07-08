6648fdbbd72c5072612cd8c0c56fa3aeb9bd2fb6
faby <fabian.kropfhamer@gmail.com>
2026-07-08 19:49:29 +0200

    cleanup

--- supabase/migrations/20250524095918_add-storage-policies.sql
+++ supabase/migrations/20250524095918_add-storage-policies.sql
@@ -14,23 +14,3 @@ for all
 to authenticated
 using ((auth.uid() = owner))
 with check ((auth.uid() = owner));
-
-
-create policy "all_cmds_for_atleast_viewer"
-on "storage"."buckets"
-as permissive
-for select
-to authenticated
-using ((EXISTS ( SELECT 1
-   FROM user_profiles_table
-  WHERE ((user_profiles_table.userid = auth.uid()) AND (user_profiles_table.userrole >= 2)))));
-
-
-create policy "all_cmds_for_atleast_viewer"
-on "storage"."objects"
-as permissive
-for select
-to authenticated
-using ((EXISTS ( SELECT 1
-   FROM user_profiles_table
-  WHERE ((user_profiles_table.userid = auth.uid()) AND (user_profiles_table.userrole >= 2)))));
--- supabase/migrations/20251007184825_create_phase_texts_table.sql
+++ supabase/migrations/20251007184825_create_phase_texts_table.sql
@@ -12,6 +12,3 @@ on public.phase_texts
 for select
 to authenticated
 using (true);
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON public.phase_texts TO service_role;
-GRANT SELECT on public.phase_texts to authenticated
